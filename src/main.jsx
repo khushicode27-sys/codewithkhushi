6053a689d931f9306b2d9be381111e155e88f3d2
CodeWithKhushi <khushicode27@gmail.com>
2026-08-09 09:29:41 -0700

    Fix CSS filename imports for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./styles/Global.css";
-import "./styles/Responsive.css";
+import "./styles/global.css";
+import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
